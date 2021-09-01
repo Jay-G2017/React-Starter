06b2aece6c83202a557beb3e34def7e0174bdce5
gaojie <gaojie@wxcloud.com>
2021-09-01 14:50:55 +0800

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -16,4 +16,14 @@ module.exports = {
     }),
   ],
   mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
 };
